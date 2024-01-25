f6d0bce0a1a3d0a2e2a998a5862310992806d00b
Pratham Modi <84131564+Kal1-linux@users.noreply.github.com>
2024-01-25 16:44:22 +0530

    Update deploy_node.sh
    done

--- k8s/deploy_node.sh
+++ k8s/deploy_node.sh
@@ -2,5 +2,7 @@ kubectl delete deployment nginx
 kubectl apply -f deployment.yaml
 kubectl delete service todo-service
 kubectl apply -f service.yaml
+kubectl delete nodeapp
+kubectl apply -f pod.yaml
 minikube ip
 ifconfig
